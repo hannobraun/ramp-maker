5f1838ad96f5739de3800b5e1ce63e30244de44d
Hanno Braun <hanno@braun-embedded.com>
2021-01-14 15:01:05 +0100

    Add Cargo package

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//! RampMaker - Stepper Acceleration Ramp Generator
+//!
+//! RampMaker is a library that generates acceleration profiles for stepper
+//! motors. It can be used independently, or together with [Step/Dir].
+//!
+//! [Step/Dir]: https://crates.io/crates/step-dir
+
+#![cfg_attr(not(test), no_std)]
+#![deny(missing_docs, broken_intra_doc_links)]
